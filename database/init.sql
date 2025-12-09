4e1144b959faeaa9649228cbe83232c07e136fbf
Abdessamad Jebari <abdessamadjebari@gmail.com>
2025-12-09 18:51:43 +0100

    feat: Initial commit for Project 2 - Two-Tier Docker Containerization

---
+++ database/init.sql
@@ -0,0 +1,8 @@
+-- two-tier-project/database/init.sql
+
+CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    task VARCHAR(255) NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
